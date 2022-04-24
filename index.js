3eab38e7de81db6f808b8cc876320a0816e5c8f7
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-04-24 13:50:52 +0200

    Products and category query

--- index.js
+++ index.js
@@ -95,18 +95,43 @@ const products = [
   },
 ];
 
+let categories = [
+  {
+    id: 'c01b1ff4-f894-4ef2-b27a-22aacc2fca70',
+    name: 'Kitchen',
+  },
+  {
+    id: '34115aac-0ff5-4859-8f43-10e8db23602b',
+    name: 'Garden',
+  },
+  {
+    id: 'd914aec0-25b2-4103-9ed8-225d39018d1d',
+    name: 'Sports',
+  },
+];
+
 const typeDefs = gql`
   type Query {
     products: [Product!]!
     product(id: ID!): Product
+    categories: [Category!]!
+    category(id: ID!): Category
   }
   type Product {
+    id: ID!
     name: String!
     description: String!
     quantity: Int!
     price: Float!
     onSale: Boolean!
     image: String!
+    category: Category!
+  }
+
+  type Category {
+    id: ID!
+    name: String!
+    products: [Product!]!
   }
 `;
 
@@ -116,10 +141,28 @@ const resolvers = {
       return products;
     },
     product: (parent, args, context) => {
-      const productID = args.id;
-      const product = products.find(({ id }) => id === productID);
-      if (!product) return null;
-      return product;
+      const { id } = args;
+      return products.find((product) => product.id === id);
+    },
+    categories: () => {
+      return categories;
+    },
+    category: (parent, args, context) => {
+      const { id } = args;
+      return categories.find((categories) => categories.id === id);
+    },
+  },
+  Category: {
+    products: (parent, args, context) => {
+      const categoryId = parent.id;
+      return products.filter((product) => product.categoryId === categoryId);
+    },
+  },
+
+  Product: {
+    category: (parent, args, context) => {
+      const { categoryId } = parent;
+      return categories.find((category) => category.id === categoryId);
     },
   },
 };
